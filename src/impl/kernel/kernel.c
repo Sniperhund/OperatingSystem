add163861813df6f09581d7a68b03d02103ff85c
Lucas Täkker <lucastakker@gmail.com>
2023-08-18 23:58:20 +0200

    Added GDT

--- src/impl/kernel/kernel.c
+++ src/impl/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include "print.h"
+#include "gdt.h"
 #include "drivers/terminal.h"
 
 void kernel_main() {
+    GDTInitialize();
+
     TerminalClear();
     
-    
+    kprintf("You are very handsome!");
 }
--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,4 +1,5 @@
 global header_start
+global start
 
 section .text
 bits 32
@@ -9,3 +10,28 @@ start:
     call kernel_main
 
     hlt
+
+global GDTLoad
+GDTLoad:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp + 8]
+    lgdt [eax]
+
+    mov eax, [ebp + 12]
+    push eax
+    push .reload_cs
+    retf
+
+.reload_cs:
+    mov ax, [ebp + 16]
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, ebp
+    pop ebp
+    ret
